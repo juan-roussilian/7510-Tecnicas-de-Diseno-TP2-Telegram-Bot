2b71289b1bab9b497ea43d20d035858b58272785
Juan <jroussilian@fi.uba.ar>
2023-06-12 21:17:14 -0300

    fix:corrijo end de final de funcion borrado accidentalmente

--- app/routes.rb
+++ app/routes.rb
@@ -64,6 +64,7 @@ class Routes
       razon = JSON.parse(respuesta.body)['error']
       bot.api.send_message(chat_id: message.chat.id, text: "No se pudo registrar, #{razon}")
     end
+  end
 
   on_message '/saldo' do |bot, message|
     endpoint = "#{ENV['API_URL']}/saldo?usuario=#{message.from.id}"
