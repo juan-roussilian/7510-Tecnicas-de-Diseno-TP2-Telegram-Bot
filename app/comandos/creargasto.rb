c1db26fd6f605e21207012af253477daf621376d
MateoCraviotto <macraviotto@fi.uba.ar>
2023-06-13 23:15:47 -0300

    fix: agrego manejo de respuesta para el comando /crear-grupo

--- app/comandos/creargasto.rb
+++ app/comandos/creargasto.rb
@@ -9,6 +9,10 @@ class ComandoCrearGasto
   def ejecutar
     endpoint = "#{ENV['API_URL']}/gasto"
     respuesta = Faraday.post(endpoint, { usuario: @usuario, nombre_gasto: @nombre_gasto, monto: @monto, nombre_grupo: @nombre_grupo }.to_json)
-    "Gasto creado id: #{JSON.parse(respuesta.body)['id']}"
+    if respuesta.status == 201
+      "Gasto creado id: #{JSON.parse(respuesta.body)['id']}"
+    else
+      JSON.parse(respuesta.body)['error']
+    end
   end
 end
